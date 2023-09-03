74b00f454ea44dd42551a7be44210b3eda75eb21
OlhaZheleva <dorarashkova@gmail.com>
2023-09-03 12:11:54 +0200

    check

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // "use strict";
 
-let game = new Phaser.Game(400, 500, Phaser.CANVAS, null, {
+let game = new Phaser.Game(280, 512, Phaser.CANVAS, null, {
   preload: preload,
   create: create,
   update: update,
@@ -10,23 +10,41 @@ var bird;
 let upKey;
 var spacebar;
 var ground;
-var pipes;
+
+let playing = false;
+let startButton;
+
+let pipesGroup;
+let gapsGroup;
+let nextPipes;
+let currentPipe;
 
 function preload() {
   game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
   game.scale.pageAlignHorizontally = true;
   game.scale.pageAlignVertically = true;
-  game.stage.backgroundColor = "#87CEEB";
 
   // ініціалізуємо елементи
   game.load.image("bird", "img/bird.png");
-  game.load.image("button", "img/up.png", 40, 40);
-  game.load.image("pipe", "img/pipe.png");
   game.load.image("ground", "img/ground.png");
+  game.load.spritesheet("button", "img/button.png");
+  game.load.image("background", "img/background.png");
+
+  // Pipes
+  game.load.image("pipeTop", "img/pipe-top.png");
+  game.load.image("pipeBottom", "img/pipe-bottom.png");
 }
 
 function create() {
   // відображаємо елементи на екрані
+
+  // фон
+  background = game.add.sprite(
+    game.world.width * 0,
+    game.world.height * 0,
+    "background"
+  );
+
   // пташка
   bird = game.add.sprite(
     game.world.width * 0.4,
@@ -37,67 +55,140 @@ function create() {
   // земля
   ground = game.add.sprite(
     game.world.width * 0,
-    game.world.height * 0,
+    game.world.height - 50,
     "ground"
   );
+
+  // кнопка "розпочати гру"
+  startButton = game.add.button(
+    game.world.width * 0.5,
+    game.world.height * 0.5,
+    "button",
+    startGame,
+    this,
+    1,
+    0,
+    2
+  );
+  startButton.anchor.set(0.5);
 
   // додаємо елементи у фізичну систему
   game.physics.startSystem(Phaser.Physics.ARCADE);
   game.physics.arcade.enable(bird);
   game.physics.arcade.enable(ground);
 
-  bird.body.gravity.y = 1000;
-
-  pipes = game.add.group();
-  pipes.enableBody = true;
-  pipes.createMultiple(20, "pipe");
-
-  timer = game.time.events.loop(1600, addLongPipes);
 
   // івент підскок пташки при натисканні на пробіл
   spacebar = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
-  spacebar.onDown.add(upBird(), this);
 
-  // івент підскок пташки при натисканні на клавішу вгору
-  upKey = game.input.keyboard.addKey(Phaser.KeyCode.UP);
-  upKey.onDown.add(upBird(), this);
+  // // івент підскок пташки при натисканні на клавішу вгору
+  // upKey = game.input.keyboard.addKey(Phaser.KeyCode._onKeyUp);
 
-  // // завершення гри
-  // bird.checkWorldBounds = true;
-  // bird.events.onOutOfBounds.add(function () {
-  //   alert("Game over!");
-  //   location.reload();
-  // }, this);
+  // завершення гри
+  bird.checkWorldBounds = true;
+  bird.events.onOutOfBounds.add(function () {
+    alert("Game over!");
+    location.reload();
+  }, this);
+
+
+  gapsGroup = game.physics.add.group();
+  pipesGroup = game.physics.add.group();
+  scoreboardGroup = game.physics.add.staticGroup();
+
+
 }
 
 function update() {
-  game.physics.arcade.collide(bird, ground);
+
+  game.physics.arcade.collide(bird, ground, gameOver);
   ground.body.immovable = true;
+
 
   if (spacebar.justDown) {
     upBird();
-  } else if (upKey.justDown) {
-    upBird();
   }
+
+  pipesGroup.children.iterate(function (child) {
+    if (child == undefined) return;
+
+    if (child.x < -50) child.destroy();
+    else child.setVelocityX(-100);
+  });
+
+  gapsGroup.children.iterate(function (child) {
+    child.body.setVelocityX(-100);
+  });
+
+  nextPipes++;
+  if (nextPipes === 130) {
+    makePipes(game.game.games[0]);
+    nextPipes = 0;
+  }
+}
+
+
+// завантаження нової гри 
+
+function prepareGame() {
+  framesMoveUp = 0
+  nextPipes = 0
+  currentPipe = assets.obstacle.pipe.green
+  score = 0
+  gameOver = false
+  backgroundDay.visible = true
+  backgroundNight.visible = false
+  messageInitial.visible = true
+
+  birdName = getRandomBird()
+  player = game.physics.add.sprite(60, 265, birdName)
+  player.setCollideWorldBounds(true)
+  player.anims.play(getAnimationBird(birdName).clapWings, true)
+  player.body.allowGravity = false
+
+  game.physics.add.collider(player, ground, hitBird, null, game)
+  game.physics.add.collider(player, pipesGroup, hitBird, null, game)
+
+  game.physics.add.overlap(player, gapsGroup, updateScore, null, game)
+
+  ground.anims.play(assets.animation.ground.moving, true)
+}
+
+// функція завершення гри
+function gameOver() {
+  alert("Game over!");
+  location.reload();
 }
 
 function upBird() {
   bird.body.velocity.set(0, -250);
 }
 
-function addShotPipe(x, y) {
-  var pipe = this.pipes.getFirstDead();
-  pipe.reset(x, y);
-  pipe.body.velocity.x = -200;
-  pipe.checkWorldBounds = true;
-  pipe.outOfBoundsKill = true;
+// функція початку гри
+function startGame() {
+  startButton.destroy();
+  bird.body.gravity.y = 1000;
+  playing = true;
+
+  // makePipes();
 }
 
-function addLongPipes() {
-  var blank = Math.floor(Math.random() * 5) + 1;
-  for (var i = 0; i < 8; i++) {
-    if (i != blank && i != blank + 1) {
-      this.addShotPipe(400, i * 60 + 10);
-    }
-  }
+function makePipes() {
+  if (!gameStarted || gameOver) return;
+
+  const pipeTopY = Phaser.Math.Between(-120, 120);
+
+  const gap = game.add.line(288, pipeTopY + 210, 0, 0, 0, 98);
+  gapsGroup.add(gap);
+  gap.body.allowGravity = false;
+  gap.visible = false;
+
+  const pipeTop = pipesGroup.create(288, pipeTopY, currentPipe.top);
+  pipeTop.body.allowGravity = false;
+
+  const pipeBottom = pipesGroup.create(288, pipeTopY + 420, currentPipe.bottom);
+  pipeBottom.body.allowGravity = false;
 }
+
+
+
